470adc74db3c1066c502805936a6aa8b829b4cb8
Jixun Wu <jixun.moe@gmail.com>
2023-06-10 00:55:10 +0100

    test: mock `matchMedia`

--- src/test-utils/setup-jest.ts
+++ src/test-utils/setup-jest.ts
@@ -15,3 +15,17 @@ if (!global.Worker) {
     }
   };
 }
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }),
+});
