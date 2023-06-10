dd48ae77782ac78b623debc975903b9703af0fa9
Jixun Wu <jixun.moe@gmail.com>
2023-06-10 17:54:06 +0100

    fix: reword footer to fit single line in mobile device (close #22)

--- src/__tests__/sanity-check.test.tsx
+++ src/__tests__/sanity-check.test.tsx
@@ -16,6 +16,6 @@ test('should be able to render App', async () => {
   await waitFor(() => screen.getByTestId('sdk-version'));
 
   // Quick sanity check of known strings.
-  expect(screen.getByText(/仅在浏览器内对文件进行解锁/i)).toBeInTheDocument();
+  expect(screen.getByText(/在浏览器内对文件进行解锁/i)).toBeInTheDocument();
   expect(screen.getByText(/UnlockMusic 团队/i)).toBeInTheDocument();
 });
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,23 +20,25 @@ export function Footer() {
       flexShrink={0}
     >
       <Flex as={Text}>
-        {'音乐解锁 (__APP_VERSION_SHORT__'}
+        <Link href="https://git.unlock-music.dev/um/um-react" isExternal>
+          音乐解锁
+        </Link>
+        {' (__APP_VERSION_SHORT__'}
         <Suspense>
           <SDKVersion />
         </Suspense>
         {') - 移除已购音乐的加密保护。'}
       </Flex>
       <Text>
-        {'Copyright © 2019 - '}
+        {'© 2019 - '}
         <CurrentYear />{' '}
         <Link href="https://git.unlock-music.dev/um" isExternal>
           UnlockMusic 团队
         </Link>
-        {' | 音乐解锁授权基于'}
+        {' | '}
         <Link href="https://git.unlock-music.dev/um/um-react/src/branch/main/LICENSE" isExternal>
-          MIT许可协议
+          使用 MIT 授权协议
         </Link>
-        。
       </Text>
     </Center>
   );
