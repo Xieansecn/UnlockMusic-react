63c1a25f53b9ac7a4060b985847bab39f7d3d6a8
Jixun Wu <jixun.moe@gmail.com>
2023-06-15 20:34:15 +0100

    feat: add ios ekey import instructions

--- src/features/settings/panels/QMCv2/ImportFileModal.tsx
+++ src/features/settings/panels/QMCv2/ImportFileModal.tsx
@@ -12,6 +12,7 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  Text,
   useToast,
 } from '@chakra-ui/react';
 
@@ -24,6 +25,8 @@ import { InstructionsAndroid } from './InstructionsAndroid';
 import { MMKVParser } from '~/util/MMKVParser';
 import { getFileName } from '~/util/pathHelper';
 import { InstructionsMac } from './InstructionsMac';
+import { InstructionsIOS } from './InstructionsIOS';
+import { InstructionsPC } from './InstructionsPC';
 
 export interface ImportFileModalProps {
   show: boolean;
@@ -88,17 +91,27 @@ export function ImportFileModal({ onClose, show }: ImportFileModalProps) {
             <FileInput onReceiveFiles={handleFileReceived}>拖放或点我选择含有密钥的数据库文件</FileInput>
           </Center>
 
-          <Flex as={Tabs} variant="enclosed" flexDir="column" mt={4} flex={1} minH={0}>
+          <Text mt={2}>选择你的「QQ 音乐」客户端平台以查看对应说明：</Text>
+
+          <Flex as={Tabs} variant="enclosed" flexDir="column" flex={1} minH={0}>
             <TabList>
-              <Tab>安卓客户端</Tab>
-              <Tab>Mac 客户端</Tab>
+              <Tab>安卓</Tab>
+              <Tab>iOS</Tab>
+              <Tab>Mac</Tab>
+              <Tab>Windows</Tab>
             </TabList>
             <TabPanels flex={1} overflow="auto">
               <TabPanel>
                 <InstructionsAndroid />
               </TabPanel>
               <TabPanel>
+                <InstructionsIOS />
+              </TabPanel>
+              <TabPanel>
                 <InstructionsMac />
+              </TabPanel>
+              <TabPanel>
+                <InstructionsPC />
               </TabPanel>
             </TabPanels>
           </Flex>
