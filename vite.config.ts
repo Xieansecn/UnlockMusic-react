af6bd3575551d494e880c0c273278c5b74be4459
Jixun Wu <jixun.moe@gmail.com>
2023-09-16 16:10:36 +0100

    chore: merge vitest config to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import url from 'node:url';
 import path from 'node:path';
 import fs from 'node:fs';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import replace from '@rollup/plugin-replace';
@@ -99,6 +99,51 @@ export default defineConfig({
           utility: ['radash', 'nanoid', 'immer', 'react-syntax-highlighter'],
         },
       },
+    },
+  },
+  test: {
+    globals: true,
+    mockReset: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-utils/setup-jest.ts'],
+    alias: [
+      {
+        find: /^~\/(.*)/,
+        replacement: 'src/$1',
+      },
+    ],
+    // workaround: sql.js is not ESModule friendly, yet...
+    deps: {
+      // inline: ['sql.js'],
+      optimizer: {
+        web: {
+          include: ['sql.js'],
+        },
+      },
+    },
+    api: {
+      port: 5174, // vite port + 1
+    },
+    coverage: {
+      provider: 'v8',
+      exclude: [
+        // default rules
+        'coverage/**',
+        'dist/**',
+        'packages/*/test{,s}/**',
+        '**/*.d.ts',
+        'cypress/**',
+        'test{,s}/**',
+        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
+
+        // custom ones
+        'src/test-utils/**',
+      ],
     },
   },
 });
