5d731d066f64a384b6a6805b945c35d2a337904c
Jixun Wu <jixun.moe@gmail.com>
2023-09-16 16:12:30 +0100

    fix: typo in linux adb dump command

--- src/components/AndroidADBPullInstruction/adb_dump.sh
+++ src/components/AndroidADBPullInstruction/adb_dump.sh
@@ -1,2 +1,2 @@
-sh adb shell su -c "cat '{{ dir }}/{{ file }}' | gzip | base64" \
-    | base64 -d | gzip -d '{{ file }}'
+adb shell su -c "cat '{{ dir }}/{{ file }}' | gzip | base64" \
+    | base64 -d | gzip -d >'{{ file }}'
